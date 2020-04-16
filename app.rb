c75f29ff59317b919ee8cb2200d5ad46fca09fce
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-16 20:08:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,6 @@
 require './environment'
-require_relative 'modules/pirate'
-require_relative 'modules/ship'
+require_relative 'app/modules/pirate.rb'
+require_relative 'app/modules/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
